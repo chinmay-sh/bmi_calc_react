5869c6c081fea73f17299da3fca90d525a651df4
Chinmay <chinmaysharma45@gmail.com>
2020-04-21 11:21:22 +0530

    Updates

---
+++ precache-manifest.4a4b911037e68cfc909f5d319a9e6d25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed945817fb1c2c5a07be42bcae9a69e",
+    "url": "/bmi_calc_react/index.html"
+  },
+  {
+    "revision": "4deb6a58888601c43989",
+    "url": "/bmi_calc_react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "60a3f114851b6e0e5912",
+    "url": "/bmi_calc_react/static/css/main.b950528a.chunk.css"
+  },
+  {
+    "revision": "4deb6a58888601c43989",
+    "url": "/bmi_calc_react/static/js/2.678b32c8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bmi_calc_react/static/js/2.678b32c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a3f114851b6e0e5912",
+    "url": "/bmi_calc_react/static/js/main.e9c877f4.chunk.js"
+  },
+  {
+    "revision": "ce03848da54bba53fb0c",
+    "url": "/bmi_calc_react/static/js/runtime-main.bc6e77e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bmi_calc_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
